0b6924d66ff02b3a39fd30d918453dd138295e2f
go-hackerscloud <gohackerscloud@gmail.com>
2025-12-09 13:55:33 +0530

    Create db.js

---
+++ app/db/db.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const dbPath = path.join(__dirname, 'database.sqlite');
+
+const db = new sqlite3.Database(dbPath);
+
+module.exports = {
+  get: (sql, params=[]) => new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => err ? reject(err) : resolve(row));
+  }),
+  all: (sql, params=[]) => new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => err ? reject(err) : resolve(rows));
+  }),
+  run: (sql, params=[]) => new Promise((resolve, reject) => {
+    db.run(sql, params, function(err) { err ? reject(err) : resolve(this); });
+  }),
+  db
+};
